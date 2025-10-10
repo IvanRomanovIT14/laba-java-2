e3312cd417e352a83a7d214990b563acfcf299fe
IvanRomanovIT14 <ivanromanov956@gmail.com>
2025-10-10 15:32:14 +0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,29 +2,29 @@ public class Person {
     private String name;
     private int height;
 
-    public Person(String name, int height) {
+    public Person(String name, int height) { //Конструктор с 2 параметрами
         this.name = name;
         this.height = height;
     }
 
     @Override
-    public String toString() {
+    public String toString() { //Метод для создания строки
         return name + height;
     }
 
-    public String getName() {
+    public String getName() { //Геттер для имени
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) { //Сеттер для имени
         this.name = name;
     }
 
-    public int getHeight() {
+    public int getHeight() { //Геттер для роста
         return height;
     }
 
-    public void setHeight(int height) {
+    public void setHeight(int height) { //Сеттер для роста
         this.height = height;
     }
 }
